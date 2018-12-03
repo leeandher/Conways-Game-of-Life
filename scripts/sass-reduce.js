ff379727c8f3d57d3877c4b9592bdcadf49befcd
Leander Rodrigues <leandergrodrigues@gmail.com>
2018-12-02 23:08:45 -0500

    set up script to save new sass files

---
+++ scripts/sass-reduce.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const path = require("path");
+
+const sassFolder = path.join(__dirname, "../src/styles/scss");
+const sassOut = "index.scss";
+
+if (fs.existsSync(`${sassFolder}/${sassOut}`)) {
+  fs.unlinkSync(`${sassFolder}/${sassOut}`);
+}
+
+const stream = fs.createWriteStream(`${sassFolder}/${sassOut}`);
+fs.readdirSync(sassFolder).forEach(fileName => {
+  stream.write(`@import "${fileName.replace(".scss", "")}";${"\n"}`);
+});
+stream.end();
+
+console.log("Sass files have been compiled successfully!");
+console.log("");
+console.log("Now watching changes to existing files");
